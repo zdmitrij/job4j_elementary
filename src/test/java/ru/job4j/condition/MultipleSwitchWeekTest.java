2a2f9367b2f87463173eae2e6e79690518dc91e0
zdmitrij <zdmitrij@yandex.ru>
2022-11-19 17:55:22 +0300

    4.6.2. Switch - matcher [#334071 #361801] имя теста Monday исправлено

--- src/test/java/ru/job4j/condition/MultipleSwitchWeekTest.java
+++ src/test/java/ru/job4j/condition/MultipleSwitchWeekTest.java
@@ -3,20 +3,18 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class MultipleSwitchWeekTest {
 
     @Test
     public void whenMondaythen1() {
-            int expected = 2;
-            String name = "Вторник";
+            int expected = 1;
+            String name = "Понедельник";
             int out = MultipleSwitchWeek.numberOfDay(name);
             Assert.assertEquals(expected, out);
     }
 
     @Test
-    public void whenMondaythen6() {
+    public void whenSaturdaythen6() {
         int expected = 6;
         String name = "Saturday";
         int out = MultipleSwitchWeek.numberOfDay(name);
