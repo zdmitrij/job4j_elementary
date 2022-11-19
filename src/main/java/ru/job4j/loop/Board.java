ae18945d79a7bada11c1f35c26e341983fe0efcc
zdmitrij <zdmitrij@yandex.ru>
2022-11-19 18:01:31 +0300

    5.3. Построить шахматную доску в псевдографике. [#13559 #362044] отступ исправлен

--- src/main/java/ru/job4j/loop/Board.java
+++ src/main/java/ru/job4j/loop/Board.java
@@ -10,7 +10,7 @@ public class Board {
                     System.out.print(" ");
                 }
             }
-                        System.out.println();
+            System.out.println();
         }
     }
 
