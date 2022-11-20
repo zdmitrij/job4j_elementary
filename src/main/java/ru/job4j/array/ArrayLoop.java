6b1e44b6c19718d96c5073aeff5c9f5f2e7b36ab
zdmitrij <zdmitrij@yandex.ru>
2022-11-20 21:00:13 +0300

    6.0.5. Массивы и цикл for-each [#333581]

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -1,13 +1,15 @@
 package ru.job4j.array;
 
+import java.sql.SQLOutput;
+
 public class ArrayLoop {
     public static void main(String[] args) {
         int[] numbers = new int[5];
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = index * 2 + 3;
         }
-        for (int index = 0; index < numbers.length; index++) {
-            System.out.println("Элемент массива с индексом " + index + " имеет значение " + numbers[index]);
+        for (int level:numbers) {
+            System.out.println(level);
         }
     }
 }
