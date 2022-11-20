ccba4ed53d8dce52d7880dddbf6e937f3db26723
zdmitrij <zdmitrij@yandex.ru>
2022-11-20 19:45:31 +0300

    5.7. Ипотека [#156315 #362261] исправлено на while

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,10 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        do {
+        while (amount > 0) {
             amount = amount + amount * percent / 100 - salary;
             year++;
-        } while (amount > 0);
+        }
         return year;
     }
 }
