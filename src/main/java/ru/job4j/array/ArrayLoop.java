d974668a0bcaeeaf8e59d8f7eef788cf17276718
zdmitrij <zdmitrij@yandex.ru>
2022-11-22 22:32:37 +0300

    6.0.4. Массивы и цикл for. [#226665 #362552] исправлена русская кодировка

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -1,15 +1,13 @@
 package ru.job4j.array;
 
-import java.sql.SQLOutput;
-
 public class ArrayLoop {
     public static void main(String[] args) {
         int[] numbers = new int[5];
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = index * 2 + 3;
         }
-        for (int level : numbers) {
-            System.out.println(level);
+        for (int index = 0; index < numbers.length; index++) {
+            System.out.println("Ячейка с индексом " + index + " содержит значение " + numbers[index]);
         }
     }
 }
