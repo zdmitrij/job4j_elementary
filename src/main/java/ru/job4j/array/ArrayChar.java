5e6a6d965765b716032a2d9d92d293071fda333c
zdmitrij <zdmitrij@yandex.ru>
2022-11-22 21:11:57 +0300

    6.4.0. Слово начинается с ... [#41585 #362875] убраны лишние строки, не нужен расчет мин.длины, откорректировн if

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,11 +3,7 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        int minimallength = word.length;
-        if (pref.length <= word.length) {
-            minimallength = pref.length;
-        }
-        for (int index = 0; index <= minimallength - 1; index++) {
+        for (int index = 0; index < pref.length; index++) {
             if (word[index] != pref[index]) {
                 result = false;
                 break;
