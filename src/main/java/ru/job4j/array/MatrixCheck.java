e794cd0ac9181bca4a035182bb0b661892db8f2c
zdmitrij <zdmitrij@yandex.ru>
2022-11-24 15:45:00 +0300

    6.7.1. Моно строка в матрице. [#214126 #363687] исправлено цикл учитывает теперь только сплошной X как true 2

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -31,14 +31,4 @@ public class MatrixCheck {
         return rsl;
     }
 
-    public static boolean isWin(char[][] board) {
-        boolean result = false;
-        for (int index = 0; index < 5; index++) {
-            if (board[index][index] == 'X') {
-                result = true;
-                break;
-            }
-        }
-        return result;
-    }
 }
