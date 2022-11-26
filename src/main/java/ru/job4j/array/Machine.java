609eb820c6320b44eb95909c33aff3aa22d106ca
zdmitrij <zdmitrij@yandex.ru>
2022-11-26 10:03:41 +0300

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#34741 #364078] переменная теперь coin , пробелы :

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,10 +8,10 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int rest = money - price;
-        for (int nominal:coins) {
-                while (rest >= nominal) {
-                rsl[size] = nominal;
-                rest = rest - nominal;
+        for (int coin : coins) {
+                while (rest >= coin) {
+                rsl[size] = coin;
+                rest = rest - coin;
                 size++;
             }
         }
