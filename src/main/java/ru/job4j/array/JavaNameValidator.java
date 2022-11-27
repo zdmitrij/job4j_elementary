896d372fb1fa1f684ee7083f62ce5e2386e8f4db
zdmitrij <zdmitrij@yandex.ru>
2022-11-27 14:53:59 +0300

    6.9. String - это массив [#504884 #364493] исправлено3 переделаны условия

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -2,19 +2,17 @@ package ru.job4j.array;
 
 public class JavaNameValidator {
 
-    @SuppressWarnings("checkstyle:InnerAssignment")
-    public static boolean isNameValid(String name) {
-        boolean rsl = true;
+     public static boolean isNameValid(String name) {
+
         if (name.isEmpty() || Character.isUpperCase(name.codePointAt(0))
                 || Character.isDigit(name.codePointAt(0))) {
             return false;
         }
+         boolean rsl = true;
         for (int i = 1; i < name.length(); i++) {
             int code = name.codePointAt(i);
-            if (isSpecialSymbol(code) || isUpperLatinLetter(code) || isLowerLatinLetter(code)
-                    || Character.isDigit(code)) {
-                rsl = true;
-            } else {
+            if (isSpecialSymbol(code) && isUpperLatinLetter(code) && isLowerLatinLetter(code)
+                    && !Character.isDigit(code)) {
                 rsl = false;
                 break;
             }
@@ -23,14 +21,14 @@ public class JavaNameValidator {
     }
 
     public static boolean isSpecialSymbol(int code) {
-        return code == 36 || code == 95;
+        return code != 36 || code != 95;
     }
 
     public static boolean isUpperLatinLetter(int code) {
-        return code >= 65 && code <= 90;
+        return code < 65 && code > 90;
     }
 
     public static boolean isLowerLatinLetter(int code) {
-        return code >= 97 && code <= 122;
+        return code < 97 && code > 122;
     }
 }
